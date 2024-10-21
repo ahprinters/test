6a68c0281625455da3ae7f1df427cea1801f2888
ahprinters <ahps323@gmail.com>
2024-10-21 19:59:28 +0600

    new commit

--- this/person.php
+++ this/person.php
@@ -1,25 +1,37 @@
 <?php
 
+// Define a class named Person
 class Person
 {
+    // Declare public properties for the class
     public $name;
     public $age;
     public $email;
 
+    // Constructor method to initialize the properties
     public function __construct($name, $age, $email)    
     {
+        // Assign the provided values to the class properties
         $this->name = $name;
         $this->age = $age;
         $this->email = $email;  
     }
+
+    // Method to concatenate and return the name, age, and email
     public function setName() {
-      return $this->name. " ". $this->age. " ". $this->email;
-    
+        return $this->name . " " . $this->age . " " . $this->email;
     }   
+
+    // Method to print a greeting message along with the concatenated string from setName()
     public function getName() {
-        echo "Hello $this->name". $this->setName();
+        echo "Hello $this->name " . $this->setName();
     }   
 }
+
+// Create a new instance of the Person class
 $person = new Person("sabbir", 22, "sabbir@.com");
+
+// Call the getName() method on the $person object
 $person->getName();
+
 ?>
