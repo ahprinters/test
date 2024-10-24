3667fca7e25ab67b20572b3bca863799fdcfb153
ahprinters <ahps323@gmail.com>
2024-10-24 19:50:07 +0600

    engine type

--- methodChaining/car.php
+++ methodChaining/car.php
@@ -1,5 +1,5 @@
 <?php
-
+include 'engine.php';
 // Define the Car class
 class Car {
     // Public properties that can be accessed from outside the class
@@ -31,68 +31,13 @@ class Car {
         return $this;
     }
 
-    // Method to set the engine property
-    public function setEngine($engine) {
-        // Assign the provided value to the engine property
-        $this->engine = $engine;
-        // Return the current object to allow method chaining
-        return $this;
-    }
-}
-
-class Engine {
-    public $type;
-
-    // Constructor
-    public function __construct($type) {
-        $this->type = $type;
-    }
-
-    public function setType($type) {
-        $this->type = $type;
-    }
-}
-
-class Car {
-    public $brand;
-    public $model;
-    public $engine;
-
-    public function __construct($brand, $model, $engine) {
-        $this->brand = $brand;
-        $this->model = $model;
-        $this->engine = $engine;
-    }
-
-    public function setBrand($brand) {
-        $this->brand = $brand;
-        return $this;
-    }
-
-    public function setModel($model) {
-        $this->model = $model;
-        return $this;
-    }
-
+    // Method to display the details of the car
     public function displayCarInfo() {
-        echo "Car: $this->brand $this->model with engine type: " . $this->engine->type . "\n";
+        echo "Car: $this->brand $this->model with engine type" . $this->engine->type . "\n";
     }
+
 }
 
-// Creating an Engine object
-$engine = new Engine("V8");
-
-// Creating a Car object
-$car = new Car("Ford", "Mustang", $engine);
-
-// Setting new brand and model
-$car->setBrand("Chevrolet")->setModel("Camaro")->engine->setType("V6");
-
-// Displaying car information
-$car->displayCarInfo();
-
-// Displaying engine type and car model
-echo "Engine Type: " . $car->engine->type . "\n";
-echo "Car Model: " . $car->model . "\n";
-
+$car = new Car("Ford", "Mustang", new Engine("V8", 500, 2.0));
+$car->setBrand("Chevrolet")->setModel("Camaro")->engine->settype("V8")->displayCarInfo;
 ?>
--- methodChaining/engine.php
+++ methodChaining/engine.php
@@ -5,17 +5,9 @@
 class Engine
 {
    public $type;
-   public $power;
-   public $volume;  
-
-   public function __construct($type, $power, $volume)
+   public function __construct($type)
    {
        $this->type = $type;
-       $this->power = $power;
-       $this->volume = $volume;     
-   }
-   public function displayDetails()
-   {
-       return "Type: " . $this->type . ", Power: " . $this->power . ", Volume: " . $this->volume;
-   }
+   }    
+
 }
