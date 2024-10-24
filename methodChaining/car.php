7790a13423009bb5334d84583131434270ce60d2
ahprinters <ahps323@gmail.com>
2024-10-24 18:24:19 +0600

    Define the Car class

--- methodChaining/car.php
+++ methodChaining/car.php
@@ -1,32 +1,44 @@
 <?php
 
+// Define the Car class
 class Car {
+    // Public properties that can be accessed from outside the class
     public $brand;
     public $model;
     public $engine;
 
+    // Constructor method to initialize the properties when a new object is created
     public function __construct($brand, $model, $engine) {
+        // Assign the provided values to the properties
         $this->brand = $brand;
         $this->model = $model;
         $this->engine = $engine;
     }
 
+    // Method to set the brand property
     public function setBrand($brand) {
+        // Assign the provided value to the brand property
         $this->brand = $brand;
+        // Return the current object to allow method chaining
         return $this;
     }
 
+    // Method to set the model property
     public function setModel($model) {
+        // Assign the provided value to the model property
         $this->model = $model;
+        // Return the current object to allow method chaining
         return $this;
     }
 
+    // Method to set the engine property
     public function setEngine($engine) {
+        // Assign the provided value to the engine property
         $this->engine = $engine;
+        // Return the current object to allow method chaining
         return $this;
     }
 }
-
 
 class Engine {
     public $type;
