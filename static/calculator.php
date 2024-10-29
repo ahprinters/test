2c944476a146925c2e971ed9b427c28b1a1eebb1
ahprinters <ahps323@gmail.com>
2024-10-29 21:22:35 +0600

    radius

--- static/calculator.php
+++ static/calculator.php
@@ -9,14 +9,29 @@ class Calculator {
     public static function multiply($a, $b) {
         return $a * $b;
     }
+
     public function testing() {
         echo "hello, We are testing";
+    }
+
+    public static function areaOfCircle($radius) {
+        return self::$PI * $radius * $radius;
+    }
+
+    public static function describeCircle($radius) {
+        $area = self::areaOfCircle($radius);
+        return "A Circle with radius $radius has an area of $area";
     }
 }
 
 // echo Calculator::add(10, 20); // Outputs: 30
 // PHP_EOL;
 // echo Calculator::multiply(10, 20); // Outputs: 200
-$p1 = new Calculator();
-$p1->testing(); 
-?> 
+// $p1 = new Calculator();
+// $p1->testing();
+// echo Calculator::areaOfCircle(10);
+echo Calculator::describeCircle(4);
+
+
+
+?>
