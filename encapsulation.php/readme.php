f86b5a0cc2b74eeb72dd892fce729b97ce1b6389
ahprinters <ahps323@gmail.com>
2024-10-30 22:20:44 +0600

    Encapsulation

---
+++ encapsulation.php/readme.php
@@ -0,0 +1,20 @@
+<?php
+// https://interactivecares.com/lessons/167-data-encapsulation-in-object-oriented-programming
+
+// Title: Understanding Data Encapsulation in PHP OOP
+// 1*
+// Agenda :
+// 1.What is Data Encapsulation in Object-Oriented Programming?
+// 2. Benefits of Data Encapsulation
+// 3.Example of Data Encapsulation in PHP
+// 4. Accessing Encapsulated Data Using Getters and Setters
+// I
+/*
+Data encapsulation is one of the fundamental principles of object-oriented programming. In simple terms, it refers to the practice of hiding the internal details of a class while exposing only the necessary parts to the outside world.
+In PHP, we achieve encapsulation by setting the visibility of class properties and methods using access modifiers:
+public: Accessible from anywhere.
+private: Accessible only within the class itself.
+protected: Accessible within the class and its subclasses.
+Benefits of Data Encapsulation
+Activate Wind
+Go to Settings to a
