50db5c84037b0a6cbd663dcbe6fe9f9770d64272
ahprinters <ahps323@gmail.com>
2024-10-31 20:36:12 +0600

    secure_payment_gateway_system

--- basicBookStoreSystem/index.php
+++ basicBookStoreSystem/index.php
@@ -5,6 +5,7 @@ require_once 'Bookstore.php'; // Include the Bookstore class
 $bookstore = new Bookstore(); // Create a new Bookstore object
 
 // Create new Book objects
+
 $book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 10.99);
 $book2 = new Book("1984", "George Orwell", 8.99, false);
 $book3 = new Book("To Kill a Mockingbird", "Harper Lee", 12.99);
