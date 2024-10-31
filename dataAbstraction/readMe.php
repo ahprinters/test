de747cf73e0bc4f540182768338d102ac2e038c4
ahprinters <ahps323@gmail.com>
2024-10-31 19:53:35 +0600

    bookStoreSystem

---
+++ dataAbstraction/readMe.php
@@ -0,0 +1,22 @@
+<?php
+
+// https://interactivecares.com/lessons/167-data-abstraction-in-object-oriented-programming
+// Title: Mastering Data Abstraction in PHP Object-Oriented Programming
+/*
+Agenda :
+1.What is Data Abstraction?
+2.Benefits of Data Abstraction
+I
+3.Implementing Data Abstraction in PHP 4.Using Abstract Classes
+5.Using Interfaces for Abstraction
+6. Real-World Example of Data Abstraction in PHP
+*/
+// Data Abstraction is one of the key principles of Object-Oriented Programming (OOP). It allows you to hide the complex implementation details of a class while exposing only the essential features to the user.
+// Think of it as focusing on "what" an object can do, rather than "how" it does it. For example, when you use a TV remote, you know which buttons to press to change channels or adjust the volume, but you don't need to understand the internal mechanisms of how the remote works.
+// Benefits of Data Abstraction:
+// expActivate Wind
+// Encapsulation of Complex Logic: Abstraction hides unnecessary details and exposes essentials.
+// In 16 Col1 SOCIA
+// Go to Settings to
+// GRIE DUD
+// Go lim
